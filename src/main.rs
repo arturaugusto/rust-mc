d071ab015ea07b57c8c914a19f20ac67506744d0
Jackson Burnette <jacksondburnette@gmail.com>
2018-09-01 17:02:09 +0000

    futures functions

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,58 @@ extern crate futures;
 #[macro_use(try_nb, block)]
 extern crate nb;
 extern crate panic_abort;
+extern crate void;
 
 use cortex_m::asm;
 use cortex_m_rt::ExceptionFrame;
 use f3::hal::delay::Delay;
 use f3::hal::prelude::*;
 use f3::hal::serial::Serial;
+use f3::hal::timer::Timer;
 use f3::hal::stm32f30x;
 use f3::led::Leds;
 use futures::{future, Async, Future};
 
 
 // use futures in serial read
-fn read<S>(serial: S) -> impl Future<Item = (S, u8), Error = S::Error>
+fn read<S>(mut serial: S) -> impl Future<Item = (S, u8), Error = S::Error>
 where
     S: embedded_hal::serial::Read<u8>,
 {
     let mut serial = Some(serial);
     future::poll_fn(move || {
         let byte = try_nb!(serial.as_mut().unwrap().read());
+
         Ok(Async::Ready((serial.take().unwrap(), byte)))
     })
 }
+
+// use futures in serial write
+fn write<S>(mut serial: S, byte: u8) -> impl Future<Item = S, Error = S::Error>
+where
+    S: embedded_hal::serial::Write<u8>,
+{
+    let mut serial = Some(serial);
+    future::poll_fn(move || {
+        try_nb!(serial.as_mut().unwrap().write(byte));
+
+        Ok(Async::Ready(serial.take().unwrap()))
+    })
+}
+
+// use futures in countdown wait
+fn wait<T>(mut timer: T) -> impl Future<Item = T, Error = void::Void>
+where
+    T: embedded_hal::timer::CountDown,
+{
+    let mut timer = Some(timer);
+    future::poll_fn(move || {
+        try_nb!(timer.as_mut().unwrap().wait());
+
+        Ok(Async::Ready(timer.take().unwrap()))
+    })
+}
+
 
 entry!(main);
 
