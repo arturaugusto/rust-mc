7fa50cd7a0bbc1b18f45b3e2243d8f146d3cce24
Jackson Burnette <jacksondburnette@gmail.com>
2018-09-02 18:01:22 +0000

    build error - concurrent loopback and roulette
    does not compile, see builderr.txt

--- src/main.rs
+++ src/main.rs
@@ -7,50 +7,55 @@ extern crate cortex_m_rt;
 extern crate embedded_hal;
 extern crate f3;
 extern crate futures;
-#[macro_use(try_nb, block)]
+#[macro_use(try_nb)]
 extern crate nb;
 extern crate panic_abort;
 extern crate void;
 
 use cortex_m::asm;
 use cortex_m_rt::ExceptionFrame;
-use f3::hal::delay::Delay;
 use f3::hal::prelude::*;
 use f3::hal::serial::Serial;
 use f3::hal::timer::Timer;
 use f3::hal::stm32f30x;
 use f3::led::Leds;
 use futures::{future, Async, Future};
+use futures::future::Loop;
+use void::Void;
 
 
 // use futures in serial read
-fn read<S>(mut serial: S) -> impl Future<Item = (S, u8), Error = S::Error>
+fn read<S, R>(tx: S, rx: R) -> impl Future<Item = (S, R, u8), Error = R::Error>
 where
-    S: embedded_hal::serial::Read<u8>,
+    S: embedded_hal::serial::Write<u8>,
+    R: embedded_hal::serial::Read<u8>,
 {
-    let mut serial = Some(serial);
+    let mut tx = Some(tx);
+    let mut rx = Some(rx);
     future::poll_fn(move || {
-        let byte = try_nb!(serial.as_mut().unwrap().read());
+        let byte = try_nb!(rx.as_mut().unwrap().read());
 
-        Ok(Async::Ready((serial.take().unwrap(), byte)))
+        Ok(Async::Ready((tx.take().unwrap(), rx.take().unwrap(), byte)))
     })
 }
 
 // use futures in serial write
-fn write<S>(mut serial: S, byte: u8) -> impl Future<Item = S, Error = S::Error>
+fn write<S, R>(tx: S, rx: R, byte: u8) -> impl Future<Item = (S, R), Error = S::Error>
 where
     S: embedded_hal::serial::Write<u8>,
+    R: embedded_hal::serial::Read<u8>,
 {
-    let mut serial = Some(serial);
+    let mut tx = Some(tx);
+    let mut rx = Some(rx);
     future::poll_fn(move || {
-        try_nb!(serial.as_mut().unwrap().write(byte));
+        try_nb!(tx.as_mut().unwrap().write(byte));
 
-        Ok(Async::Ready(serial.take().unwrap()))
+        Ok(Async::Ready((tx.take().unwrap(), rx.take().unwrap())))
     })
 }
 
 // use futures in countdown wait
-fn wait<T>(mut timer: T) -> impl Future<Item = T, Error = void::Void>
+fn wait<T>(timer: T) -> impl Future<Item = T, Error = Void>
 where
     T: embedded_hal::timer::CountDown,
 {
@@ -66,7 +71,6 @@ where
 entry!(main);
 
 fn main() -> ! {
-    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
@@ -74,9 +78,10 @@ fn main() -> ! {
 
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    let _delay = Delay::new(cp.SYST, clocks);
+    let timer = Timer::tim6(dp.TIM6, 10.hz(), clocks, &mut rcc.apb1);
 
-    let _leds = Leds::new(dp.GPIOE.split(&mut rcc.ahb));
+    let leds = Leds::new(dp.GPIOE.split(&mut rcc.ahb));
+    let numleds = leds.len();
 
     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
 
@@ -85,30 +90,31 @@ fn main() -> ! {
 
     let serial =
         Serial::usart1(dp.USART1, (tx, rx), 115_200.bps(), clocks, &mut rcc.apb2);
+    let (tx, rx) = serial.split();
 
-    let (mut tx, mut rx) = serial.split();
-    let mut recieved: u8;
+    // loopback task
+    let mut loopback = future::loop_fn((tx, rx), |(tx, rx)| {
+        read(tx, rx)
+            .and_then(|(tx, rx, byte)| write(tx, rx, byte))
+            .map(|(tx, rx)| Loop::Continue((tx, rx)))
+    });
+
+    // roulette task
+    let mut roulette = future::loop_fn(
+        (leds, timer, 1),
+        |(mut leds, timer, nextled)| {
+            wait(timer).map(move |timer| {
+                leds[nextled].on();
+                leds[nextled - 1].off();
+
+                Loop::Continue((leds, timer, (nextled + 1) % numleds))
+            })
+        });
 
     loop {
-        // read a byte
-        recieved = block!(rx.read()).unwrap();
-        // write it back
-        block!(tx.write(recieved));
+        roulette.poll().unwrap();
+        loopback.poll().unwrap();
     }
-
-// comment out to focus on serial echo server
-// will move to concurrent serial and led ops
-//    let ms = 50_u8;
-//    loop {
-//        for curr in 0..8 {
-//            let next = (curr + 1) % 8;
-//
-//            leds[next].on();
-//            delay.delay_ms(ms);
-//            leds[curr].off();
-//            delay.delay_ms(ms);
-//        }
-//    }
 }
 
 exception!(HardFault, hard_fault);
